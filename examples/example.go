3d56f5828abeb7ec2471b4a499d782833e962950
Andreas Louca <andreas@louca.org>
2012-08-08 14:52:25 +0300

    Added example file
    Signed-off-by: Andreas Louca <andreas@louca.org>

---
+++ examples/example.go
@@ -0,0 +1,30 @@
+// Copyright 2012 Andreas Louca. All rights reserved.
+// Use of this source code is goverend by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/alouca/gosnmp"
+)
+
+var (
+	cmdCommunity string
+	cmdTarget    string
+	cmdOid       string
+)
+
+func init() {
+	flag.StringVar(&cmdTarget, "target", "", "Target SNMP Agent")
+	flag.StringVar(&cmdCommunity, "community", "", "SNNP Community")
+	flag.StringVar(&cmdOid, "oid", "", "OID")
+	flag.Parse()
+}
+
+func main() {
+	s := gosnmp.NewGoSNMP(cmdTarget, cmdCommunity, 1)
+	resp := s.Get(cmdOid)
+	fmt.Printf("%s -> %s\n", cmdOid, resp)
+}
