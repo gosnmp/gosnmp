f83c87fb0cdbaf4830e41960f0c904d927d41c9f
Andreas Louca <andreas@louca.org>
2012-09-18 09:33:23 +0300

    Updated example to reflect changes in the API
    Signed-off-by: Andreas Louca <andreas@louca.org>

--- examples/example.go
+++ examples/example.go
@@ -5,6 +5,7 @@
 package main
 
 import (
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"github.com/alouca/gosnmp"
@@ -14,10 +15,13 @@ var (
 	cmdCommunity string
 	cmdTarget    string
 	cmdOid       string
+	cmdDebug     string
 	cmdTimeout   int64
 )
 
 func init() {
+	flag.StringVar(&cmdDebug, "debug", "", "Debug flag expects byte array of raw packet to test decoding")
+
 	flag.StringVar(&cmdTarget, "target", "", "Target SNMP Agent")
 	flag.StringVar(&cmdCommunity, "community", "public", "SNNP Community")
 	flag.StringVar(&cmdOid, "oid", "", "OID")
@@ -26,13 +30,38 @@ func init() {
 }
 
 func main() {
+	if cmdDebug != "" {
+		fmt.Printf("Running in debug mode\n")
+		s, err := gosnmp.NewGoSNMP("", "", gosnmp.Version2c, 5)
+		s.SetDebug(true)
+		s.SetVerbose(true)
+		packet, err := hex.DecodeString(cmdDebug)
+		if err != nil {
+			fmt.Printf("Unable to decode raw packet: %s\n", err.Error())
+			return
+		}
+
+		pckt, err := s.Debug(packet)
+
+		if err != nil {
+			fmt.Printf("Error while debugging: %s\n", err.Error())
+		} else {
+			for _, resp := range pckt.Variables {
+				fmt.Printf("%s -> %v\n", resp.Name, resp.Value)
+			}
+		}
+
+		return
+	}
+
 	if cmdTarget == "" || cmdOid == "" {
 		flag.PrintDefaults()
 		return
 	}
 
 	s, err := gosnmp.NewGoSNMP(cmdTarget, cmdCommunity, gosnmp.Version2c, cmdTimeout)
-
+	s.SetDebug(true)
+	s.SetVerbose(true)
 	if err != nil {
 		fmt.Printf("Error creating SNMP instance: %s\n", err.Error())
 		return
@@ -41,21 +70,23 @@ func main() {
 	s.SetTimeout(cmdTimeout)
 	fmt.Printf("Getting %s\n", cmdOid)
 	resp, err := s.Get(cmdOid)
-
 	if err != nil {
-		fmt.Printf("Error during SNMP GET: %s\n", err.Error())
+		fmt.Printf("Error getting response: %s\n", err.Error())
 	} else {
-		fmt.Printf("%s -> ", cmdOid)
-		switch resp.Type {
-		case gosnmp.OctetString:
-			if s, ok := resp.Value.(string); ok {
-				fmt.Printf("%s\n", s)
-			} else {
-				fmt.Printf("Response is not a string\n")
+		for _, v := range resp.Variables {
+			fmt.Printf("%s -> ", v.Name)
+			switch v.Type {
+			case gosnmp.OctetString:
+				if s, ok := v.Value.(string); ok {
+					fmt.Printf("%s\n", s)
+				} else {
+					fmt.Printf("Response is not a string\n")
+				}
+			default:
+				fmt.Printf("Type: %d - Value: %v\n", v.Type, v.Value)
 			}
-		default:
-			fmt.Printf("Type: %d - Value: %v\n", resp.Type, resp.Value)
 		}
+
 	}
 
 }
