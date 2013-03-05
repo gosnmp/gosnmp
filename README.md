c0e609d568c24c2a540eb627905c470541eb8fa9
Sonia Hamilton <sonia@snowfrog.net>
2013-03-05 12:52:54 +1100

    verax testing, doco

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Install
 The easiest way to install is via go get:
 
     go get github.com/alouca/gosnmp
-  
+
 License
 -------
 
@@ -24,7 +24,7 @@ The library usage is pretty simple:
 
     s := gosnmp.NewGoSNMP("192.168.0.1", "public", gosnmp.Version2c)
     resp, err := s.Get(".1.3.6.1.2.1.1.1.0")
-    
+
     if err == nil {
       switch resp.Type {
         case OctetString:
@@ -41,7 +41,7 @@ Responses are a struct of the following format:
       Type  Asn1BER
       Value interface{}
     }
-    
+
 Where Name is the OID encoded as an object identifier, Type is the encoding type of the response and Value is an interface{} type, with the response appropriately decoded.
 
 SNMP BER Types can be one of the following:
@@ -49,18 +49,67 @@ SNMP BER Types can be one of the following:
     type Asn1BER byte
 
     const (
-      Integer          Asn1BER = 0x02
-    	BitString                = 0x03
-    	OctetString              = 0x04
-    	Null                     = 0x05
-    	ObjectIdentifier         = 0x06
-    	Counter32                = 0x41
-    	Gauge32                  = 0x42
-    	TimeTicks                = 0x43
-    	Opaque                   = 0x44
-    	NsapAddress              = 0x45
-    	Counter64                = 0x46
-    	Uinteger32               = 0x47
+        EndOfContents    Asn1BER = 0x00
+        Boolean                  = 0x01
+        Integer                  = 0x02
+        BitString                = 0x03
+        OctetString              = 0x04
+        Null                     = 0x05
+        ObjectIdentifier         = 0x06
+        ObjectDesription         = 0x07
+        IpAddress                = 0x40
+        Counter32                = 0x41
+        Gauge32                  = 0x42
+        TimeTicks                = 0x43
+        Opaque                   = 0x44
+        NsapAddress              = 0x45
+        Counter64                = 0x46
+        Uinteger32               = 0x47
+        NoSuchObject             = 0x80
+        NoSuchInstance           = 0x81
     )
-    
+
 GoSNMP supports most of the above values, subsequent releases will support all of them.
+
+Testing
+-------
+
+Many, many thanks to Andreas Louca for writing **alouca/gosnmp**. The major
+difference between his version and **soniah/gosnmp** is that the latter has
+tests written. (However the code could do with refactoring). The tests were
+used to find and correct errors in the following SNMP BER Types:
+
+* Counter32
+* Gauge32
+* Counter64
+* OctetString
+* ObjectIdentifier
+* IpAddress
+
+Also, this version contains functions for treating the returned snmp values as
+`*big.Int` (convenient, as SNMP can return int32, uint32, and uint64 values):
+
+    func ToBigInt(value interface{}) *big.Int
+
+Running the Tests
+-----------------
+
+The tests use the Verax Snmp Simulator [1]; setup Verax before running "go test":
+
+* download, install and run Verax with the default configuration
+
+* in the gosnmp directory, setup these symlinks (or equivalents for your system):
+
+    ln -s /usr/local/vxsnmpsimulator/device device
+    ln -s /usr/local/vxsnmpsimulator/conf/devices.conf.xml devices.conf.xml
+
+* remove randomising elements from Verax device files:
+
+    cd device/cisco
+    sed -i -e 's!\/\/\$.*!!' -e 's!^M!!' cisco_router.txt
+    sed -i -e 's/\/\/\^int.unq()\^\/\//2/' cisco_router.txt
+    cd ../os
+    sed -i -e 's!\/\/\$.*!!' -e 's!^M!!' os-linux-std.txt
+    sed -i -e 's/\/\/\^int.unq()\^\/\//2/' os-linux-std.txt
+
+[1] http://www.veraxsystems.com/en/products/snmpsimulator
---
+++ gosnmp_test.go
@@ -0,0 +1,271 @@
+package gosnmp
+
+import (
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+var veraxDevices = []struct {
+	path string
+	port uint16
+}{
+	{"device/os/os-linux-std.txt", 161},
+	{"device/cisco/cisco_router.txt", 162},
+}
+
+func TestGet(t *testing.T) {
+	for i, test := range veraxDevices {
+		var err error
+
+		// load verax results
+		var vresults testResults
+		if vresults, err = ReadVeraxResults(test.path); err != nil {
+			t.Errorf("#%d, |%s|: ReadVeraxResults error: |%s|", i, test.path, err)
+		}
+
+		// load gosnmp results
+		var gresults = make(testResults)
+		x, err := NewGoSNMP("127.0.0.1", test.port, "public", Version2c, 2)
+		if err != nil {
+			t.Errorf("%s, err |%s| NewGoSNMP()", test.path, err)
+		} else {
+			defer x.Conn.Close()
+		}
+		for oid, _ := range vresults {
+			if packet, err := x.Get(oid); err == nil {
+				gresults[oid] = packet
+			} else {
+				t.Errorf("%s, err |%s| Get() for oid |%s|", test.path, err, oid)
+			}
+		}
+
+		// compare results
+		for oid, vpacket := range vresults {
+			if len(vpacket.Variables) < 1 {
+				t.Errorf("%s, vpacket.Variables < 1 for oid |%s|", test.path, oid)
+			}
+			vpdu := vpacket.Variables[0]
+			vtype := vpdu.Type
+			vvalue := vpdu.Value
+
+			gpacket := gresults[oid]
+			if gpacket == nil || len(gpacket.Variables) < 1 {
+				t.Errorf("%s, gpacket.Variables < 1 for oid |%s|", test.path, oid)
+				continue
+			}
+			gpdu := gpacket.Variables[0]
+			gtype := gpdu.Type
+			gvalue := gpdu.Value
+
+			// the actual comparison testing
+			if vtype != gtype {
+				t.Errorf("vtype |%#x| doesn't match gtype |%#x| for oid |%s|", vtype, gtype, oid)
+				continue
+			}
+
+			switch vtype {
+			case Integer, Gauge32, Counter32, TimeTicks, Counter64:
+				vval := ToBigInt(vvalue)
+				gval := ToBigInt(gvalue)
+				if vval.Cmp(gval) != 0 {
+					t.Errorf("vvalue |%v|%s| doesn't match gvalue |%v|%s| for type |%#x| oid |%s|",
+						vvalue, vval, gvalue, gval, vtype, oid)
+				}
+			case OctetString:
+				var vval, gval string
+				var ok bool
+				if vval, ok = vvalue.(string); !ok {
+					t.Errorf("failed string assert vvalue |%v|", vval)
+				} else if gval, ok = gvalue.(string); !ok {
+					t.Errorf("failed string assert gvalue |%v|", gval)
+
+				} else if strings.HasPrefix(vval, "2010-") {
+					// skip weird Verax encoded hex strings
+					continue
+				} else if strings.HasPrefix(vval, "2011-") {
+					// skip weird Verax encoded hex strings
+					continue
+				} else if vval != gval && oid != "1.3.6.1.2.1.1.1.0" {
+					// Verax mishandles 1.3.6.1.2.1.1.1.0 on Cisco device
+					t.Errorf("failed string comparison\nVVAL |%s|\nGVAL |%s|\ntype |%#x| oid |%s|",
+						vval, gval, vtype, oid)
+				}
+			case ObjectIdentifier, IpAddress:
+				var vval, gval string
+				var ok bool
+				if vval, ok = vvalue.(string); !ok {
+					t.Errorf("failed string assert vvalue |%v|", vval)
+				} else if gval, ok = gvalue.(string); !ok {
+					t.Errorf("failed string assert gvalue |%v|", gval)
+				} else if vval != gval {
+					t.Errorf("failed comparison\nVVAL |%s|\nGVAL |%s|\ntype |%#x| oid |%s|",
+						vval, gval, vtype, oid)
+				}
+			default:
+				t.Errorf("unhandled case: vtype |%#x| vvalue |%v| oid |%s|", vtype, vvalue, oid)
+			}
+
+		}
+	}
+}
+
+type testResults map[string]*SnmpPacket
+
+func ReadVeraxResults(filename string) (results testResults, err error) {
+	var lines []byte
+	if lines, err = ioutil.ReadFile(filename); err != nil {
+		return nil, fmt.Errorf("unable to open file %s", filename)
+	}
+	results = make(testResults)
+
+	// some lines have newlines in them, therefore can't just split on newline
+	lines_split := re_split(regexp.MustCompile(`\n\.`), string(lines), -1)
+LINE:
+	for _, line := range lines_split {
+		splits_a := strings.SplitN(line, " = ", 2)
+		oid := splits_a[0]
+		splits_b := strings.SplitN(splits_a[1], ": ", 2)
+		oidtype := splits_b[0]
+		oidval := strings.TrimSpace(splits_b[1])
+
+		// removing leading . first oid
+		if string(oid[0]) == "." {
+			oid = oid[1:]
+		}
+
+		var pdu SnmpPDU
+		switch oidtype {
+
+		// listed in order of RFC2578
+
+		case "INTEGER":
+			if value, err := strconv.ParseInt(oidval, 10, 64); err != nil {
+				panic(fmt.Sprintf("Err converting integer. oid: %s err: %v", oid, err))
+			} else {
+				pdu.Type = Integer
+				pdu.Value = value
+			}
+
+		case "STRING", "String":
+			oidval = strings.Trim(oidval, `"`)
+			oidval = strings.Replace(oidval, string(0x0d), "", -1)
+			pdu.Type = OctetString
+			pdu.Value = oidval
+
+		case "Hex-STRING":
+			// TODO - ran out of time...
+			continue LINE
+
+		case "OID":
+			pdu.Type = ObjectIdentifier
+			pdu.Value = oidval
+
+		case "BITS":
+			// TODO - ran out of time...
+			continue LINE
+
+		case "IpAddress", "Network Address":
+			pdu.Type = IpAddress
+			pdu.Value = oidval
+			if strings.Contains(oidval, ":") {
+				// IpAddress is in "C0:A8:C4:01" format
+				octets := strings.Split(oidval, ":")
+				for i, octet := range octets {
+					n, _ := strconv.ParseUint(octet, 16, 8)
+					octets[i] = fmt.Sprintf("%d", n)
+				}
+				pdu.Value = strings.Join(octets, ".")
+			}
+
+		case "Counter32":
+			if value, err := strconv.ParseInt(oidval, 10, 64); err != nil {
+				panic(fmt.Sprintf("Err converting integer. oid: %s err: %v", oid, err))
+			} else {
+				pdu.Type = Counter32
+				pdu.Value = value
+			}
+
+		case "Gauge32":
+			if value, err := strconv.ParseInt(oidval, 10, 64); err != nil {
+				panic(fmt.Sprintf("Err converting integer. oid: %s err: %v", oid, err))
+			} else {
+				pdu.Type = Gauge32
+				pdu.Value = value
+			}
+
+		case "Timeticks":
+			matches := regexp.MustCompile(`\d+`).FindAllString(oidval, 1) // pull out "(value)"
+			oidval := matches[0]
+			if value, err := strconv.ParseInt(oidval, 10, 64); err != nil {
+				panic(fmt.Sprintf("Err converting integer. oid: %s err: %v", oid, err))
+			} else {
+				pdu.Type = TimeTicks
+				pdu.Value = value
+			}
+
+		case "Counter64":
+			if value, err := strconv.ParseUint(oidval, 10, 64); err != nil {
+				panic(fmt.Sprintf("Err converting integer. oid: %s err: %v", oid, err))
+			} else {
+				pdu.Type = Counter64
+				pdu.Value = value
+			}
+
+		default:
+			panic(fmt.Sprintf("Unhandled type: %s, %s\n", oidtype, oidval))
+		}
+
+		packet := &SnmpPacket{
+			Variables: []SnmpPDU{pdu},
+		}
+		results[oid] = packet
+	}
+	return results, nil
+}
+
+// adapted from http://codereview.appspot.com/6846048/
+//
+// re_split slices s into substrings separated by the expression and returns a slice of
+// the substrings between those expression matches.
+//
+// The slice returned by this method consists of all the substrings of s
+// not contained in the slice returned by FindAllString(). When called on an exp ression
+// that contains no metacharacters, it is equivalent to strings.SplitN().
+// Example:
+// s := regexp.MustCompile("a*").re_split("abaabaccadaaae", 5)
+// // s: ["", "b", "b", "c", "cadaaae"]
+//
+// The count determines the number of substrings to return:
+// n > 0: at most n substrings; the last substring will be the unsplit remaind er.
+// n == 0: the result is nil (zero substrings)
+// n < 0: all substrings
+func re_split(re *regexp.Regexp, s string, n int) []string {
+	if n == 0 {
+		return nil
+	}
+	if len(s) == 0 {
+		return []string{""}
+	}
+	matches := re.FindAllStringIndex(s, n)
+	strings := make([]string, 0, len(matches))
+	beg := 0
+	end := 0
+	for _, match := range matches {
+		if n > 0 && len(strings) >= n-1 {
+			break
+		}
+		end = match[0]
+		if match[1] != 0 {
+			strings = append(strings, s[beg:end])
+		}
+		beg = match[1]
+	}
+	if end != len(s) {
+		strings = append(strings, s[beg:])
+	}
+	return strings
+}
