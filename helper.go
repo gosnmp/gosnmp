14015a21b3f3979f4e7bb06a0615407c0dd5adf1
10074432 <liu.xuefeng1@zte.com.cn>
2019-02-14 18:33:31 +0800

    Revise the bug for func parseObjectIdentifier

--- helper.go
+++ helper.go
@@ -613,7 +613,9 @@ func parseLength(bytes []byte) (length int, cursor int) {
 // that are assigned in a hierarchy.
 func parseObjectIdentifier(bytes []byte) (s []int, err error) {
 	if len(bytes) == 0 {
-		err = fmt.Errorf("zero length OBJECT IDENTIFIER")
+		//err = fmt.Errorf("zero length OBJECT IDENTIFIER")
+		s = make([]int, 1)
+		s[0] = 0
 		return
 	}
 
