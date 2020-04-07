e20a42544469c62db8be7207f6f2e4c6bd882f89
Ian Pye <pye@kentik.com>
2020-04-07 16:03:03 -0700

    adding full debug into into the backtrace of snmp failure

--- marshal.go
+++ marshal.go
@@ -13,7 +13,8 @@ import (
 	"fmt"
 	"io"
 	"net"
-	"runtime"
+	"reflect"
+	"runtime/debug"
 	"strings"
 	"sync/atomic"
 	"time"
@@ -406,10 +407,7 @@ func (x *GoSNMP) sendOneRequest(packetOut *SnmpPacket,
 func (x *GoSNMP) send(packetOut *SnmpPacket, wait bool) (result *SnmpPacket, err error) {
 	defer func() {
 		if e := recover(); e != nil {
-			var buf = make([]byte, 8192)
-			runtime.Stack(buf, true)
-
-			err = fmt.Errorf("recover: %v Stack:%v", e, string(buf))
+			err = fmt.Errorf("recover: stacktrace from panic: \n" + string(debug.Stack()))
 		}
 	}()
 
