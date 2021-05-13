2868fe490bb218597a58a12bb6937f9276f63fd7
ygbillet <yg.billet+github@gmail.com>
2021-05-13 18:16:52 +0200

    Fix SNMPv3 trap authentication (#332)
    This fix corrects the SNMPv3 implementation for traps according to :
    - RFC3414 for MD5 and SHA1
    - RFC7860 for SHA2 (SHA-224, SHA-256, SHA-384, SHA-512)
    
    Fixes #284
    
    Signed-off-by: Yves-Gaël Billet <yg.billet@gmail.com>

--- v3_usm.go
+++ v3_usm.go
@@ -15,11 +15,11 @@ import (
 	"crypto/cipher"
 	"crypto/des" //nolint:gosec
 	"crypto/hmac"
-	_ "crypto/md5" //nolint:gosec
+	"crypto/md5" //nolint:gosec
 	crand "crypto/rand"
-	_ "crypto/sha1" //nolint:gosec
-	_ "crypto/sha256"
-	_ "crypto/sha512"
+	"crypto/sha1"     //nolint:gosec
+	_ "crypto/sha256" // Register hash function #4 (SHA224), #5 (SHA256)
+	_ "crypto/sha512" // Register hash function #6 (SHA384), #7 (SHA512)
 	"encoding/binary"
 	"encoding/hex"
 	"errors"
@@ -621,31 +621,102 @@ func (sp *UsmSecurityParameters) discoveryRequired() *SnmpPacket {
 	return nil
 }
 
-func (sp *UsmSecurityParameters) calcPacketDigest(packet []byte) []byte {
-	var mac hash.Hash
+func (sp *UsmSecurityParameters) calcPacketDigest(packet []byte) ([]byte, error) {
+	return calcPacketDigest(packet, sp)
+}
 
-	switch sp.AuthenticationProtocol {
-	default:
-		mac = hmac.New(crypto.MD5.New, sp.SecretKey)
-	case SHA:
-		mac = hmac.New(crypto.SHA1.New, sp.SecretKey)
-	case SHA224:
-		mac = hmac.New(crypto.SHA224.New, sp.SecretKey)
-	case SHA256:
-		mac = hmac.New(crypto.SHA256.New, sp.SecretKey)
-	case SHA384:
-		mac = hmac.New(crypto.SHA384.New, sp.SecretKey)
-	case SHA512:
-		mac = hmac.New(crypto.SHA512.New, sp.SecretKey)
+// calcPacketDigest calculate authenticate digest for incoming messages (TRAP or
+// INFORM).
+// Support MD5, SHA1, SHA224, SHA256, SHA384, SHA512 protocols
+func calcPacketDigest(packetBytes []byte, secParams *UsmSecurityParameters) ([]byte, error) {
+	var digest []byte
+	var err error
+
+	switch secParams.AuthenticationProtocol {
+	case MD5, SHA:
+		digest, err = digestRFC3414(
+			secParams.AuthenticationProtocol,
+			packetBytes,
+			secParams.SecretKey)
+	case SHA224, SHA256, SHA384, SHA512:
+		digest, err = digestRFC7860(
+			secParams.AuthenticationProtocol,
+			packetBytes,
+			secParams.SecretKey)
 	}
 
-	_, _ = mac.Write(packet)
+	return digest, err
+}
+
+// digestRFC7860 calculate digest for incoming messages using HMAC-SHA2 protcols
+// according to RFC7860 4.2.2
+func digestRFC7860(h SnmpV3AuthProtocol, packet []byte, authKey []byte) ([]byte, error) {
+	mac := hmac.New(h.HashType().New, authKey)
+	_, err := mac.Write(packet)
+	if err != nil {
+		return []byte{}, err
+	}
 	msgDigest := mac.Sum(nil)
-	return msgDigest
+	return msgDigest, nil
+}
+
+// digestRFC3414 calculate digest for incoming messages using MD5 or SHA1
+// according to RFC3414 6.3.2 and 7.3.2
+func digestRFC3414(h SnmpV3AuthProtocol, packet []byte, authKey []byte) ([]byte, error) {
+	var extkey [64]byte
+	var err error
+	var k1, k2 [64]byte
+	var h1, h2 hash.Hash
+
+	copy(extkey[:], authKey)
+
+	switch h {
+	case MD5:
+		h1 = md5.New() //nolint:gosec
+		h2 = md5.New() //nolint:gosec
+	case SHA:
+		h1 = sha1.New() //nolint:gosec
+		h2 = sha1.New() //nolint:gosec
+	}
+
+	for i := 0; i < 64; i++ {
+		k1[i] = extkey[i] ^ 0x36
+		k2[i] = extkey[i] ^ 0x5c
+	}
+
+	_, err = h1.Write(k1[:])
+	if err != nil {
+		return []byte{}, err
+	}
+
+	_, err = h1.Write(packet)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	d1 := h1.Sum(nil)
+
+	_, err = h2.Write(k2[:])
+	if err != nil {
+		return []byte{}, err
+	}
+
+	_, err = h2.Write(d1)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return h2.Sum(nil)[:12], nil
 }
 
 func (sp *UsmSecurityParameters) authenticate(packet []byte) error {
-	msgDigest := sp.calcPacketDigest(packet)
+	var msgDigest []byte
+	var err error
+
+	if msgDigest, err = sp.calcPacketDigest(packet); err != nil {
+		return err
+	}
+
 	idx := bytes.Index(packet, macVarbinds[sp.AuthenticationProtocol])
 
 	if idx < 0 {
@@ -658,6 +729,7 @@ func (sp *UsmSecurityParameters) authenticate(packet []byte) error {
 
 // determine whether a message is authentic
 func (sp *UsmSecurityParameters) isAuthentic(packetBytes []byte, packet *SnmpPacket) (bool, error) {
+	var msgDigest []byte
 	var packetSecParams *UsmSecurityParameters
 	var err error
 
@@ -665,7 +737,9 @@ func (sp *UsmSecurityParameters) isAuthentic(packetBytes []byte, packet *SnmpPac
 		return false, err
 	}
 	// TODO: investigate call chain to determine if this is really the best spot for this
-	msgDigest := sp.calcPacketDigest(packetBytes)
+	if msgDigest, err = calcPacketDigest(packetBytes, packetSecParams); err != nil {
+		return false, err
+	}
 
 	for k, v := range []byte(packetSecParams.AuthenticationParameters) {
 		if msgDigest[k] != v {
