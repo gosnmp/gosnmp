c63cba9a48d90fe2e1969c4580d8885dd1be6fd4
Ivan Torgashov <it1804@mail.ru>
2021-05-14 22:01:04 +0500

    * helper.go: removing unused member Name of variable struct * preallocate []SnmpPDU slice in Get() function
    Signed-off-by: Ivan Torgashov <it1804@mail.ru>

--- helper.go
+++ helper.go
@@ -22,9 +22,8 @@ import (
 	"strconv"
 )
 
-// variable struct is used by decodeValue(), which is used for debugging
+// variable struct is used by decodeValue()
 type variable struct {
-	Name  []int
 	Type  Asn1BER
 	Value interface{}
 }
