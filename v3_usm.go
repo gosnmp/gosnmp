4087aeaabc41d51d5996355d8a2579e7745149c5
Balasankar Rajaguru <sankar.bala@gmail.com>
2021-10-09 14:07:27 +0530

    Validate SNMPv3 Auth/Priv Protocol for incoming trap message (#351)
    * fix auth priv check
    
    Signed-off-by: Balasankar Rajaguru <balasank@juniper.net>

--- v3_usm.go
+++ v3_usm.go
@@ -978,6 +978,11 @@ func (sp *UsmSecurityParameters) unmarshal(flags SnmpV3MsgFlags, packet []byte, 
 	}
 	// blank msgAuthenticationParameters to prepare for authentication check later
 	if flags&AuthNoPriv > 0 {
+		// In case if the authentication protocol is not configured or set to NoAuth, then the packet cannot
+		// be processed further
+		if sp.AuthenticationProtocol <= NoAuth {
+			return 0, errors.New("error parsing SNMPv3 User Security Model: authentication parameters are not configured to parse incoming authenticated message")
+		}
 		copy(packet[cursor+2:cursor+len(macVarbinds[sp.AuthenticationProtocol])], macVarbinds[sp.AuthenticationProtocol][2:])
 	}
 	cursor += count
@@ -990,6 +995,11 @@ func (sp *UsmSecurityParameters) unmarshal(flags SnmpV3MsgFlags, packet []byte, 
 	if msgPrivacyParameters, ok := rawMsgPrivacyParameters.(string); ok {
 		sp.PrivacyParameters = []byte(msgPrivacyParameters)
 		sp.Logger.Printf("Parsed privacyParameters %s", msgPrivacyParameters)
+		if flags&AuthPriv >= AuthPriv {
+			if sp.PrivacyProtocol <= NoPriv {
+				return 0, errors.New("error parsing SNMPv3 User Security Model: privacy parameters are not configured to parse incoming encrypted message")
+			}
+		}
 	}
 
 	return cursor, nil
